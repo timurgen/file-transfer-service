c351e45d66cff1edf23969099bb1fb472a93c3a1
Timur Samkharadze <timur.samkharadze@sysco.no>
2019-08-27 10:55:24 +0200

    implemented fail fast on file download/upload error code refactoring

--- service/service.py
+++ service/service.py
@@ -1,10 +1,16 @@
-import requests
+"""
+Simple service to download files from URL's extracted from Sesam data entities and upload
+them to another URL
+"""
 import os
 import tempfile
 import logging
 import json
 
+import requests
 from flask import Flask, Response, request
+
+from str_utils import str_to_bool
 
 APP = Flask(__name__)
 """Property name in entities in incoming data that contains url to file to be downloaded"""
@@ -14,6 +20,8 @@ FILE_NAME = os.environ.get('FILE_NAME', 'file_id')
 CHUNK_SIZE = os.environ.get('CHUNK_SIZE', 262144 * 4 * 10)
 
 UPLOAD_URL = os.environ.get('UPLOAD_URL')
+
+FAIL_FAST_ON_ERROR = str_to_bool(os.environ.get('FAIL_FAST_ON_ERROR', 'false'))
 
 if not UPLOAD_URL:
     logging.error("Target endpoint not defined. Check UPLOAD_URL env. variable  in your config.")
@@ -35,39 +43,47 @@ def process():
         file_name = input_entity[FILE_NAME]
         local_path = input_entity['local_path']
 
-        logging.info("processing request for {}".format(file_name))
+        logging.info(f"processing request for {file_name}")
 
         try:
             res = requests.get(file_url, stream=True)
             res.raise_for_status()
             file_path = download_file(res)
 
-            logging.debug("Starting upload file {} to {}".format(file_name, UPLOAD_URL))
+            logging.debug(f"Starting upload file {file_name} to {UPLOAD_URL}")
 
             file_to_upload = open(file_path, 'rb')
             requests.post(UPLOAD_URL, files={file_name: (file_name, file_to_upload)},
                           headers={"local_path": local_path})
 
-            logging.debug("Deleting temporary file {}".format(file_path))
+            logging.debug(f"Deleting temporary file {file_path}")
 
             file_to_upload.close()
             input_entity['transfer_service'] = "TRANSFERRED"
-        except Exception as e:
-            input_entity['transfer_service'] = "ERROR: {}".format(str(e))
+        except Exception as exc:
+            input_entity['transfer_service'] = f"ERROR: {exc}"
+            if FAIL_FAST_ON_ERROR:
+                raise exc
         finally:
             if file_path:
                 os.remove(file_path)
     return Response(json.dumps(input_data), content_type='application/json')
 
 
-def download_file(res):
+def download_file(res: requests.Response) -> str:
+    """
+    function that downloads a binary content of input response object and
+    stores into NamedTemporaryFile in file system
+    :param res: requests.Response instance
+    :return: stored file name (path)
+    """
     logging.debug("Chunked file download started")
     with tempfile.NamedTemporaryFile(delete=False) as file:
         for chunk in res.iter_content(chunk_size=CHUNK_SIZE):
             if chunk:
                 file.write(chunk)
         file.close()
-    logging.debug("File stored as {}".format(file.name))
+    logging.debug(f"File stored as {file.name}")
     return file.name
 
 
